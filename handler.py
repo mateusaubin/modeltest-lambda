03ec4ce8b6a8a501748638f9a0b30f8167863aee
mateusaubin <mateusaubin@users.noreply.github.com>
2018-09-07 18:28:21 -0300

    calling phyml help

--- handler.py
+++ handler.py
@@ -1,8 +1,11 @@
 import json
+import os
+import subprocess
 
+libdir = os.path.join(os.getcwd(), 'lib', 'phyml')
 
 def execute(event, context):
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
+    result = subprocess.run([libdir, "--help"], stdout=subprocess.PIPE)
+    return { 
+        'result': "%s" % result.stdout 
     }
