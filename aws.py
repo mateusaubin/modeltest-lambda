bcba45355ba20ecd1bf0c421aef626aa8436fa26
mateusaubin <mateusaubin@users.noreply.github.com>
2018-09-08 14:42:27 -0300

    separate SNS specific code into aws module

---
+++ aws.py
@@ -0,0 +1,39 @@
+import boto3
+import uuid
+import json
+import os
+from pathlib import Path
+import shutil
+
+class SNSInterface:
+
+    def __del__(self):
+        if bool(os.getenv('IS_LOCAL', False)):
+            print('delete temp')
+            shutil.rmtree(self.tmp_folder)
+        print("goodbye temp data")
+
+    def __init__(self, sns_record):
+        self.file_info = self.parse(sns_record)
+        self.s3 = boto3.client('s3')
+
+
+    @staticmethod
+    def parse(record):
+        message = { 'data' : record['Sns']['Message'], 'run_id' : record['Sns']['Subject'] }
+        payload = json.loads(message['data'])
+        filedata = payload.pop('path').split('://')
+        finfo = { 'bucket': filedata[0], 'key': filedata[1] }
+
+        print(payload)
+        return finfo
+
+
+    def download(self):
+        tmp_guid = str(uuid.uuid4())
+        self.tmp_folder = os.path.join('/tmp', tmp_guid)
+        os.mkdir(self.tmp_folder)
+        download_path = os.path.join('/tmp', tmp_guid, Path(self.file_info['key']).name)
+
+        print(download_path)
+        self.s3.download_file(self.file_info['bucket'], self.file_info['key'], download_path)
--- handler.py
+++ handler.py
@@ -1,15 +1,8 @@
-import json
-import subprocess
-import boto3
 import os
-import sys
-import uuid
-from pathlib import Path  
-import time
-import shutil
+import json
+import aws
 
-s3 = boto3.client('s3')
-libdir = os.path.join(os.getcwd(), 'lib', 'phyml')
+#libdir = os.path.join(os.getcwd(), 'lib', 'phyml')
 
 def execute(event, context):
     #result = subprocess.run([libdir, "--help"], stdout=subprocess.PIPE)
@@ -21,32 +14,14 @@ def execute(event, context):
     #           source_dir + "/", "s3://" + to_bucket + "/"]
     #print(subprocess.check_output(command, stderr=subprocess.STDOUT))
     for record in event['Records']:
+        sns = aws.SNSInterface(record)
+        sns.download()
 
-        # PARSE
-        message = { 'data' : record['Sns']['Message'], 'run_id' : record['Sns']['Subject'] }
-        payload = json.loads(message['data'])
-        filedata = payload.pop('path').split('://')
-        finfo = {'bucket':filedata[0], 'key':filedata[1]}
-
-        # DOWNLOAD
-        tmp_guid = str(uuid.uuid4())
-        tmp_folder = os.path.join('/tmp', tmp_guid)
-        os.mkdir(tmp_folder)
-        download_path = os.path.join('/tmp', tmp_guid, Path(finfo['key']).name)
-
-        print(download_path)
-        print(payload)
-
-        s3.download_file(finfo['bucket'], finfo['key'], download_path)
+        
       
 
 #upload_path = '/tmp/resized-{}'.format(key)
 #s3_client.upload_file(upload_path, '{}resized'.format(bucket), key)
-
-        if bool(os.getenv('IS_LOCAL', False)):
-            time.sleep(1)
-            print('delete temp')
-            shutil.rmtree(tmp_folder)
 
         print('fez algum super processamento')
 
