e2484b69d5e1951327643ac7a9e32c9fc8a5bdfb
mateusaubin <mateusaubin@users.noreply.github.com>
2018-09-08 13:42:58 -0300

    fix indent and add local debug path

--- handler.py
+++ handler.py
@@ -16,38 +16,44 @@ def execute(event, context):
     #return { 
     #    'result': "%s" % result.stdout 
     #}
-#import subprocess
-#command = ["./aws", "s3", "sync", "--acl", "public-read", "--delete",
-#           source_dir + "/", "s3://" + to_bucket + "/"]
-#print(subprocess.check_output(command, stderr=subprocess.STDOUT))
-  for record in event['Records']:
+    #import subprocess
+    #command = ["./aws", "s3", "sync", "--acl", "public-read", "--delete",
+    #           source_dir + "/", "s3://" + to_bucket + "/"]
+    #print(subprocess.check_output(command, stderr=subprocess.STDOUT))
+    for record in event['Records']:
 
-      # PARSE
-      message = { 'data' : record['Sns']['Message'], 'run_id' : record['Sns']['Subject'] }
-      payload = json.loads(message['data'])
-      filedata = payload.pop('path').split('://')
-      finfo = {'bucket':filedata[0], 'key':filedata[1]}
+        # PARSE
+        message = { 'data' : record['Sns']['Message'], 'run_id' : record['Sns']['Subject'] }
+        payload = json.loads(message['data'])
+        filedata = payload.pop('path').split('://')
+        finfo = {'bucket':filedata[0], 'key':filedata[1]}
 
-      # DOWNLOAD
-      tmp_guid = str(uuid.uuid4())
-      tmp_folder = os.path.join('/tmp', tmp_guid)
-      os.mkdir(tmp_folder)
-      download_path = os.path.join('/tmp', tmp_guid, Path(finfo['key']).name)
+        # DOWNLOAD
+        tmp_guid = str(uuid.uuid4())
+        tmp_folder = os.path.join('/tmp', tmp_guid)
+        os.mkdir(tmp_folder)
+        download_path = os.path.join('/tmp', tmp_guid, Path(finfo['key']).name)
 
-      print(download_path)
-      print(payload)
+        print(download_path)
+        print(payload)
 
-      s3.download_file(finfo['bucket'], finfo['key'], download_path)
+        s3.download_file(finfo['bucket'], finfo['key'], download_path)
       
 
 #upload_path = '/tmp/resized-{}'.format(key)
 #s3_client.upload_file(upload_path, '{}resized'.format(bucket), key)
 
-      if os.getenv('IS_LOCAL', False):
-        time.sleep(1)
-        print('delete temp')
-        shutil.rmtree(tmp_folder)
+        if os.getenv('IS_LOCAL', False):
+            time.sleep(1)
+            print('delete temp')
+            shutil.rmtree(tmp_folder)
 
-      print('fez algum super processamento')
+        print('fez algum super processamento')
 
-  return { 'msg': "-- THE END -- " }
+    return { 'msg': "-- THE END -- " }
+
+
+if os.getenv('IS_LOCAL', False):
+    with open('sns.json') as f:
+        data = json.load(f)
+        execute(data,None)
