c87ab7e9fae279825c18973c413bda15c33a1e87
mateusaubin <mateusaubin@users.noreply.github.com>
2018-09-08 14:47:05 -0300

    format with autopep

--- aws.py
+++ aws.py
@@ -5,6 +5,7 @@ import os
 from pathlib import Path
 import shutil
 
+
 class SNSInterface:
 
     def __del__(self):
@@ -17,23 +18,24 @@ class SNSInterface:
         self.file_info = self.parse(sns_record)
         self.s3 = boto3.client('s3')
 
-
     @staticmethod
     def parse(record):
-        message = { 'data' : record['Sns']['Message'], 'run_id' : record['Sns']['Subject'] }
+        message = {'data': record['Sns']['Message'],
+                   'run_id': record['Sns']['Subject']}
         payload = json.loads(message['data'])
         filedata = payload.pop('path').split('://')
-        finfo = { 'bucket': filedata[0], 'key': filedata[1] }
+        finfo = {'bucket': filedata[0], 'key': filedata[1]}
 
         print(payload)
         return finfo
-
 
     def download(self):
         tmp_guid = str(uuid.uuid4())
         self.tmp_folder = os.path.join('/tmp', tmp_guid)
         os.mkdir(self.tmp_folder)
-        download_path = os.path.join('/tmp', tmp_guid, Path(self.file_info['key']).name)
+        download_path = os.path.join(
+            '/tmp', tmp_guid, Path(self.file_info['key']).name)
 
         print(download_path)
-        self.s3.download_file(self.file_info['bucket'], self.file_info['key'], download_path)
+        self.s3.download_file(
+            self.file_info['bucket'], self.file_info['key'], download_path)
--- handler.py
+++ handler.py
@@ -4,31 +4,30 @@ import aws
 
 #libdir = os.path.join(os.getcwd(), 'lib', 'phyml')
 
+
 def execute(event, context):
     #result = subprocess.run([libdir, "--help"], stdout=subprocess.PIPE)
-    #return { 
-    #    'result': "%s" % result.stdout 
-    #}
+    # return {
+    #    'result': "%s" % result.stdout
+    # }
     #import subprocess
-    #command = ["./aws", "s3", "sync", "--acl", "public-read", "--delete",
+    # command = ["./aws", "s3", "sync", "--acl", "public-read", "--delete",
     #           source_dir + "/", "s3://" + to_bucket + "/"]
     #print(subprocess.check_output(command, stderr=subprocess.STDOUT))
     for record in event['Records']:
         sns = aws.SNSInterface(record)
         sns.download()
 
-        
-      
 
 #upload_path = '/tmp/resized-{}'.format(key)
 #s3_client.upload_file(upload_path, '{}resized'.format(bucket), key)
 
         print('fez algum super processamento')
 
-    return { 'msg': "-- THE END -- " }
+    return {'msg': "-- THE END -- "}
 
 
 if bool(os.getenv('IS_LOCAL', False)) & bool(os.getenv('VSCODE', False)):
     with open('sns.json') as f:
         data = json.load(f)
-        execute(data,None)
+        execute(data, None)
